a721ca2cea826c8264120020407ff5bc21e82a6a
Giovanna Oruro <orurog@gmail.com>
2017-04-05 00:12:39 -0500

    se agrego habilidades y nav bar

--- js/main.js
+++ js/main.js
@@ -1,20 +1,7 @@
-/*function progress(){
-    var prg = document.getElementById('progress');
-    var percent = document.getElementById('percentCount');
-    var counter = 5;
-    var progress = 25;
-    var id = setInterval(frame, 50);
-    
-    function frame(){
-        if (progress == 500 & counter == 100){
-            clearInterval(id);
-        }else{
-            progress += 5;
-            counter += 1;
-            prg.style.width = progress +'px';
-            percent.innerHTML = counter +'%';
-        }
-    }
-    
-}
-progress();*/
+jQuery(document).ready(function(){
+	jQuery('.skillbar').each(function(){
+		jQuery(this).find('.skillbar-bar').animate({
+			width:jQuery(this).attr('data-percent')
+		},6000);
+	});
+});
